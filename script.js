c840ff9e650785320ec5bf2073931a19473b1c56
Halge Sakshi <sakshihalge2000@gmail.com>
2021-09-01 21:15:17 +0530

    updated styles

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello")
+const cardcontent = document.querySelector(".card-content")
+const acc_header = document.querySelectorAll(".accordion-header img")
+const collapse = document.querySelectorAll(".accordion-collapse")
+
+// acc_header.onClick = () => {
+// }
+
+acc_header[1].addEventListener('click', () => {
+    console.log("ggsg");
+    collapse[1].classList.toggle("active-accordion-collapse")
+    cardcontent.classList.toggle("opened-collapse");
+})
